6a3312ecc16bc92aa08b93e4d1a83b826c1cc367
Ahmad Beirkdar <ahmadb@protonmail.com>
2020-09-21 15:27:51 -0400

    Update Three Number Sum.cpp

--- Arrays/Three Number Sum.cpp
+++ Arrays/Three Number Sum.cpp
@@ -1,25 +1,15 @@
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
+using namespace std;
 
-std::vector<std::vector<int>> threeNumberSum(std::vector<int> array, int targetSum) {
-	std::sort(array.begin(),array.end());
-	std::vector<vector<int>> ans; 
-	
-	for(int i = 0; i < array.size()-2; i++){
-		auto left = array.begin() + i + 1;
-		auto right = array.end() -1;
-		while(*left < *right){
-			if(array.at(i) + *left + *right == targetSum){
-				ans.push_back({array.at(i), *left, *right});
-				left++;right--;
-			}
-			else if(array.at(i) + *left + *right < targetSum)
-				left++;
-			else if(array.at(i) + *left + *right > targetSum)
-				right--;
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+	std::unordered_set<int> num_set (array.begin(),array.end());
+  for(auto i : num_set){
+		int test = targetSum - i;
+		if(num_set.count(test) && test != i){
+			return vector<int> {i,test};
 		}
 	}
-  return ans;
+  return {};
 }
-
 
