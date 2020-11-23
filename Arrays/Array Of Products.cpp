82f8313034913464c45f967e5d29ad036e498e40
Ahmad Beirkdar <ahmadb@protonmail.com>
2020-11-22 22:26:09 -0500

    weird formating

--- Arrays/Array Of Products.cpp
+++ Arrays/Array Of Products.cpp
@@ -2,13 +2,12 @@
 #include <algorithm>
 
 std::vector<int> arrayOfProducts(std::vector<int> array) {
-  std::vector<int> ans(array.size());
+    std::vector<int> ans(array.size());
 	auto j = 0;
 	
 	std::for_each(ans.begin(),ans.end(),[&j,&array](int &i){
 		i = std::accumulate(array.begin(),array.end(),1,[&array,&j](const int &a, const int&b){
 			return (&(array.at(j)) == &b) ? a : a * b;
 		});j++;});
-
-  return ans;
+    return ans;
 }
