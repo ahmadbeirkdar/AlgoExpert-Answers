3b948ca29f5181f6ec3f79d0c523f4e42eead6d3
Ahmad Beirkdar <ahmadb@protonmail.com>
2020-11-22 22:24:06 -0500

    Array Of Products Question

---
+++ Arrays/Array Of Products.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+
+std::vector<int> arrayOfProducts(std::vector<int> array) {
+  std::vector<int> ans(array.size());
+	auto j = 0;
+	
+	std::for_each(ans.begin(),ans.end(),[&j,&array](int &i){
+		i = std::accumulate(array.begin(),array.end(),1,[&array,&j](const int &a, const int&b){
+			return (&(array.at(j)) == &b) ? a : a * b;
+		});j++;});
+
+  return ans;
+}
